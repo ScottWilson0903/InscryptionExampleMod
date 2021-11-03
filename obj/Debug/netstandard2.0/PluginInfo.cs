4dc56f70eb13f3dd893012387fb7a1bdbf53c6da
Scott Wilson <scottwilson0903@gmail.com>
2021-11-03 08:25:35 +0000

    cleaning repo

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CardLoaderMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CardLoaderMod";
-        public const string PLUGIN_NAME = "CardLoaderMod";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
